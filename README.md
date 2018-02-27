f08f9cb2b26a2744a53c135a567f1f7d22ca6339
Nikita Chernyi <developer.nikus@gmail.com>
2018-02-27 12:23:11 +0200

    Added cookie storage

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Auth module
 
+[![Build Status](https://travis-ci.org/frameworkwtf/auth.svg?branch=master)](https://travis-ci.org/frameworkwtf/auth) [![Coverage Status](https://coveralls.io/repos/frameworkwtf/auth/badge.svg?branch=master&service=github)](https://coveralls.io/github/frameworkwtf/auth?branch=master)
+
+[Documentation](https://framework.wtf/auth)
+
 # TODO
 
-* Auth by cookie (by device, without login/password)
 * Social auth
-
---
+++ src/Auth/Storage/Cookie.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wtf\Auth\Storage;
+
+use Wtf\Root;
+
+class Cookie extends Root implements StorageInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setUser(Root $user)
+    {
+        if (!class_exists(\Dflydev\FigCookies\Cookie::class)) {
+            throw new \Exception('wtf/auth cookie storage requires dflydev/fig-cookies package installed');
+        }
+
+        return \Dflydev\FigCookies\Cookie::create('user_id', $user->getId())
+                                                                  ->rememberForever()
+                                                                  ->withDomain(getenv('APP_HOST'))
+                                                                  ->withHttpOnly(true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUser($storage = null): ?Root
+    {
+        if (!class_exists(\Dflydev\FigCookies\Cookie::class)) {
+            throw new \Exception('wtf/auth cookie storage requires dflydev/fig-cookies package installed');
+        }
+
+        if ($id = \Dflydev\FigCookies\FigRequestCookies::get($storage, 'user_id', null)->getValue()) {
+            $entity = $this->entity($this->config('auth.entity'));
+            if ($entity->has(['id' => $id])) {
+                return $entity->load($id);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isLoggedIn(): bool
+    {
+        if (!class_exists(\Dflydev\FigCookies\Cookie::class)) {
+            throw new \Exception('wtf/auth cookie storage requires dflydev/fig-cookies package installed');
+        }
+
+        return (bool) \Dflydev\FigCookies\FigRequestCookies::get($storage, 'user_id', null)->getValue();
+    }
+}
